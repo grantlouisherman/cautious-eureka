3364115ac5c774c6fe9eb46abf842e3aebf26dd6
Grant Herman <gherma@meta.com>
2026-05-01 18:14:29 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,43 @@
-#[derive(Debug)] 
+#[derive(Debug, Clone)] 
 enum LineType {
     VarDecl,
     Expr
 }
-#[derive(Debug)] 
+#[derive(Debug, Clone)] 
 enum TokenType {
   VarType(String),
   VarName(String),
   VarValueNum(String),
   VarValueString(String),
+  ExprAdd(String, String),
+  ExprSub(String, String),
+
 }
-#[derive(Debug)] 
+#[derive(Debug, Clone)] 
 struct Token {
     line_num: usize,
     line_type: LineType,
-    tokens: Vec<TokenType>
+    tokens: [TokenType; 3]
 }
 
-const RESERVED_NAMES: [String; 3] = ["int", "string"];
+const RESERVED_NAMES: [&str; 2]= ["int", "string"];
 
 fn compile(code_string: String) -> Vec<Token> {
     let code: Vec<_> = code_string.split('\n').collect();
     let mut tokens: Vec<Token> = vec![];
     for (idx, value) in code.iter().enumerate() {
-        let line = value.split(" ");
+        let line:Vec<_> = value.split(" ").collect();
+        println!("{:?}", line);
+        if line[0] == "int" {
+            tokens.push(Token{
+                line_num: idx,
+                line_type: LineType::VarDecl,
+                tokens: [
+                TokenType::VarType(line[0].to_string()), 
+                TokenType::VarName(line[1].to_string()), 
+                TokenType::VarValueNum(line[3].to_string())]
+            }) 
+        }
         
     }
     tokens

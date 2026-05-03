6dc578a237aafb7e0d4ba777f54862392b211f36
grantlouisherman <grantlouisherman041@gmail.com>
2026-05-03 00:08:38 -0400

    things are broken

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+/*
+ * expression -> variable_decl | equation | literal
+ * literal -> NUMBER | STRING
+ * variable_decl -> var_type literal "=" literal
+ * var_type -> int | str 
+ * equation -> binary
+ *  binary -> literal operator literal
+ *  operator -> "+"
+ *
+ * */
+
+
 #[derive(Debug, Clone)] 
 enum LineType {
     VarDecl,
-    Expr
+    Equation
 }
 #[derive(Debug, Clone)] 
 enum TokenType {
@@ -9,15 +21,18 @@ enum TokenType {
   VarName(String),
   VarValueNum(String),
   VarValueString(String),
-  ExprAdd(String, String),
-  ExprSub(String, String),
+  EqAdd(String, String),
+  EqSub(String, String),
+  LiteralNum(String),
+  LiteralString(String),
+  Operator(String),
 
 }
 #[derive(Debug, Clone)] 
 struct Token {
     line_num: usize,
     line_type: LineType,
-    tokens: [TokenType; 3]
+    tokens: Vec<TokenType>
 }
 
 const RESERVED_NAMES: [&str; 2]= ["int", "string"];
@@ -27,7 +42,6 @@ fn compile(code_string: String) -> Vec<Token> {
     let mut tokens: Vec<Token> = vec![];
     for (idx, value) in code.iter().enumerate() {
         let line:Vec<_> = value.split(" ").collect();
-        println!("{:?}", line);
         if line[0] == "int" {
             tokens.push(Token{
                 line_num: idx,
@@ -35,15 +49,24 @@ fn compile(code_string: String) -> Vec<Token> {
                 tokens: [
                 TokenType::VarType(line[0].to_string()), 
                 TokenType::VarName(line[1].to_string()), 
-                TokenType::VarValueNum(line[3].to_string())]
+                TokenType::VarValueNum(line[3].to_string().replace(";", ""))].to_vec()
             }) 
         }
-        
+        if value.contains("+") {
+            tokens.push(Token{
+                line_num: idx,
+                line_type: LineType::Equation,
+                tokens: [
+                TokenType::VarType(line[0].to_string()), 
+                TokenType::VarName(line[1].to_string()), 
+                TokenType::VarValueNum(line[3].to_string().replace(";", ""))].to_vec()
+            });
+            }
     }
     tokens
 }
 fn main() {
-  let tokens = compile(String::from("int x = 5;\nint y = 4;"));
+  let tokens = compile(String::from("int x = 5;\nint y = 4;\n1+2;"));
   for token in tokens {
       println!("{:?}", token);
   }

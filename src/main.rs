765369ba0e1f689b3650a0f105dcaab884b8b508
Grant Herman <grantlouisherman041@gmail.com>
2026-05-05 19:15:10 -0400

    using lalrpop

--- src/main.rs
+++ src/main.rs
@@ -8,35 +8,10 @@
  *  operator -> "+"
  *
  * */
+use lalrpop_util::lalrpop_mod;
 
-use scnr::ScannerBuilder;
-
-static PATTERNS: &[&str] = &[
-    r";",                    // Semicolon
-    r"0|[1-9][0-9]*",        // Number
-    r"//.*(\r\n|\r|\n)",     // Line comment
-    r"/\*([^*]|\*[^/])*\*/", // Block comment
-    r"[a-zA-Z_]\w*",         // Identifier
-    r"=",                    // Assignment
-];
-
-const INPUT: &str = r#"
-// This is a comment
-int a = 10;
-int b = 20;
-string y = "hello";
-/* This is a block comment
-   that spans multiple lines */
-int c = a;
-"#;
+lalrpop_mod!(pub scanner);
 
 fn main() {
-    let scanner = ScannerBuilder::new()
-        .add_patterns(PATTERNS)
-        .build()
-        .expect("ScannerBuilder error");
-    let find_iter = scanner.find_iter(INPUT);
-    for ma in find_iter {
-        println!("Match: {:?}: '{}'", ma, &INPUT[ma.span().range()]);
-    }
+    assert!(scanner::TermParser::new().parse("22").is_ok());
 }

a8ddce90edf25e02a533d28b27880d6b79a3b3e6
Grant Herman <grantlouisherman041@gmail.com>
2026-05-05 17:48:34 -0400

    scanner

--- src/main.rs
+++ src/main.rs
@@ -9,65 +9,38 @@
  *
  * */
 
+use scnr::ScannerBuilder;
 
-#[derive(Debug, Clone)] 
-enum LineType {
-    VarDecl,
-    Equation
-}
-#[derive(Debug, Clone)] 
-enum TokenType {
-  VarType(String),
-  VarName(String),
-  VarValueNum(String),
-  VarValueString(String),
-  EqAdd(String, String),
-  EqSub(String, String),
-  LiteralNum(String),
-  LiteralString(String),
-  Operator(String),
+static PATTERNS: &[&str] = &[
+    r";",
+    r"0|[1-9][0-9]*",
+    r"=",
+    r""
+];
 
-}
-#[derive(Debug, Clone)] 
-struct Token {
-    line_num: usize,
-    line_type: LineType,
-    tokens: Vec<TokenType>
-}
-
-const RESERVED_NAMES: [&str; 2]= ["int", "string"];
-
-fn compile(code_string: String) -> Vec<Token> {
-    let code: Vec<_> = code_string.split('\n').collect();
-    let mut tokens: Vec<Token> = vec![];
-    for (idx, value) in code.iter().enumerate() {
-        let line:Vec<_> = value.split(" ").collect();
-        if line[0] == "int" {
-            tokens.push(Token{
-                line_num: idx,
-                line_type: LineType::VarDecl,
-                tokens: [
-                TokenType::VarType(line[0].to_string()), 
-                TokenType::VarName(line[1].to_string()), 
-                TokenType::VarValueNum(line[3].to_string().replace(";", ""))].to_vec()
-            }) 
-        }
-        if value.contains("+") {
-            tokens.push(Token{
-                line_num: idx,
-                line_type: LineType::Equation,
-                tokens: [
-                TokenType::VarType(line[0].to_string()), 
-                TokenType::VarName(line[1].to_string()), 
-                TokenType::VarValueNum(line[3].to_string().replace(";", ""))].to_vec()
-            });
-            }
-    }
-    tokens
-}
 fn main() {
-  let tokens = compile(String::from("int x = 5;\nint y = 4;\n1+2;"));
-  for token in tokens {
-      println!("{:?}", token);
-  }
+    let code = r#"
+        int x = 100;
+        int y = 2;
+        string hello = "hello";
+    "#;
+    let split:Vec<_> = code.split("\n").collect();
+    
+    let scanner = ScannerBuilder::new()
+        .add_patterns(PATTERNS)
+        .build()
+        .expect("ScannerBuilder error");
+    let mut tokens = vec![];
+    for(_, line) in split.iter().enumerate() {
+        let find_iter = scanner.find_iter(line);
+        for _ma in find_iter {
+            tokens.push(_ma);
+        }
+    }
+
+    for token in tokens {
+        println!("{:?}", token);
+
+    }
+
 }
